4b693c97b7e544345e30f18e385e069aa195c69e
Lincoln-Dupree <lincoln.vfx@gmail.com>
2025-09-13 09:23:08 -0500

    added another icon for the share click so we have light/dark options for the click events.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const shareBtn = document.querySelector(".share-btn");
 const popUp = document.querySelector(".share-popup-container");
+const shareIcon = document.querySelector(".share-img");
 
 shareBtn.addEventListener('click', function () {
     popUp.classList.toggle("show-me");
+})
+
+shareBtn.addEventListener('mousedown', function () {
+    shareIcon.src = 'images/icon-share-white.png';
+})
+
+shareBtn.addEventListener('mouseup', function () {
+    shareIcon.src = 'images/icon-share.svg';
 })
